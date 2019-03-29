326c90a366d825f7c4e2e7ba5f000ab21db9363d
Mauran Muthiah <mmu@zitcom.dk>
2019-03-29 11:29:13 +0100

    Removed gatewayid since its not required anymore

--- src/OnPayAPI.php
+++ src/OnPayAPI.php
@@ -64,7 +64,6 @@ class OnPayAPI {
         $requiredOptions = [
             'client_id',
             'redirect_uri',
-            'gateway_id'
         ];
 
         $missing = array_diff_key(array_flip($requiredOptions), $options);
