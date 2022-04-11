1e2eb1e953c0c9263976be799ca85fa1cdd685b5
mike-brookes <mbircohoakells@me.com>
2022-04-11 15:33:53 +0100

    Alphabetize Method Enum list

--- src/API/Util/PaymentMethods/Enums/Methods.php
+++ src/API/Util/PaymentMethods/Enums/Methods.php
@@ -3,15 +3,15 @@
 namespace OnPay\API\Util\PaymentMethods\Enums;
 
 final class Methods {
-    const CARD = 'card';
-    const MOBILEPAY_CHECKOUT = 'mobilepay_checkout';
     const ANYDAY = 'anyday';
+    const APPLE_PAY = 'applepay';
+    const CARD = 'card';
+    const GOOGLE_PAY = 'googlepay';
+    const MOBILEPAY = 'mobilepay';
+    const MOBILEPAY_CHECKOUT = 'mobilepay_checkout';
     const PAYPAL = 'paypal';
     const SWISH = 'swish';
     const VIABILL = 'viabill';
-    const APPLE_PAY = 'applepay';
-    const GOOGLE_PAY = 'googlepay';
-    const MOBILEPAY = 'mobilepay';
     const VIPPS = 'vipps';
 
     private function __construct() {
