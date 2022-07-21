81b6bbf42820327a532d207629f07939aa96b46d
Dennis Væversted <dennis.vaeversted@team.blue>
2022-07-21 21:37:27 +0200

    Add PayPal as method constant

--- src/API/PaymentWindow.php
+++ src/API/PaymentWindow.php
@@ -16,6 +16,7 @@ class PaymentWindow
     const METHOD_GOOGLEPAY = 'googlepay';
     const METHOD_VIPPS = 'vipps';
     const METHOD_SWISH = 'swish';
+    const METHOD_PAYPAL = 'paypal';
 
     const DELIVERY_DISABLED_NO_REASON = 'no-reason';
     const DELIVERY_DISABLED_NOT_PHYSICAL = 'not-physical';
