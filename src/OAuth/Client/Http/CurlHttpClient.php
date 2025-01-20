422a70e9c40f89e1e57249cdfab2080ae57ffad0
Tom Sommer <tomsommer@users.noreply.github.com>
2025-01-20 10:21:52 +0100

    Raise CURLOPT_TIMEOUT to 30
    Some acquirers take longer than 10 seconds to process

--- src/OAuth/Client/Http/CurlHttpClient.php
+++ src/OAuth/Client/Http/CurlHttpClient.php
@@ -89,7 +89,7 @@ class CurlHttpClient implements HttpClientInterface
         $defaultCurlOptions = [
             CURLOPT_HEADER => false,
             CURLOPT_CONNECTTIMEOUT => 5,
-            CURLOPT_TIMEOUT => 10,
+            CURLOPT_TIMEOUT => 30,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_FOLLOWLOCATION => false,
             CURLOPT_PROTOCOLS => $this->allowHttp ? CURLPROTO_HTTPS | CURLPROTO_HTTP : CURLPROTO_HTTPS,
